f9d52d09e910d8987c6baefaa8819f5f1efc5549
Berta Verges <bverges@fursan.com.sa>
2021-11-08 22:19:50 +0100

    features make request fulfill request and learn request

--- meeseeks/src/meeseeks.js
+++ meeseeks/src/meeseeks.js
@@ -30,5 +30,27 @@ function randomMessageOnRequest() {
     Math.floor(Math.random() * this.messageOnRequest.length)
   ];
 }
+// Closure to make request but it will not be brought to action (executed) 
+//  until it's fulfilled (method fulfillRequest)
+MrMeeseeks.prototype.makeRequest = function(desire, object) { 
+  let closure = function(thing) {
+      function execute() {
+          return desire + " " + thing;
+      }
+      return execute;
+  };
+// property attached mr meeseeks ( current object) 
+// here we define what the action will be!
+  this.action = closure(object); 
+  this.speakOnRequest();
+};
+
+MrMeeseeks.prototype.fulfillRequest = function() {
+  console.log(this.action() + " All done!!");
+}
+
+MrMeeseeks.prototype.learnRequest = function(desire, object) {
+  this.action = desire(object);
+};
 
 exports.singleMrMeeseeks = factory;
